a8c265c671c10b782e7e6c84e4cc94e016afc603
Badu <david77115@gmail.com>
2019-06-12 18:31:50 +0800

    test: rotate card animation

--- app/src/main/java/com/crazyma/batuanimlab/CardFlipActivity.kt
+++ app/src/main/java/com/crazyma/batuanimlab/CardFlipActivity.kt
@@ -1,12 +1,22 @@
 package com.crazyma.batuanimlab
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_card_flig.*
 
 class CardFlipActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_flig)
+    }
+
+    fun buttonClicked(v: View){
+
+        textView.animate().apply {
+            rotationY(360f)
+            duration = 1000
+        }.start()
     }
 }
