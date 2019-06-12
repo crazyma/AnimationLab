77df1b5d8fea78713ce83a1107ac6d7cb9ef33ca
Badu <david77115@gmail.com>
2019-06-12 17:52:31 +0800

    feat: first commit

---
+++ app/src/main/java/com/crazyma/batuanimlab/CardFlipActivity.kt
@@ -0,0 +1,12 @@
+package com.crazyma.batuanimlab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CardFlipActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_flig)
+    }
+}
