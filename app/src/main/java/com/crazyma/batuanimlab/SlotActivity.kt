773d32f3b26daf063ecf5ba210701ac51f91254d
Badu <david77115@gmail.com>
2019-06-13 12:08:13 +0800

    feat: create SlotActivity

---
+++ app/src/main/java/com/crazyma/batuanimlab/SlotActivity.kt
@@ -0,0 +1,13 @@
+package com.crazyma.batuanimlab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SlotActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slot)
+    }
+
+}
