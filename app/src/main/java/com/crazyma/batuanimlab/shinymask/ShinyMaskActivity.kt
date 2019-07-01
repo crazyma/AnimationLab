30fe531e6c5409ab8fc6de97607de630cdeafe82
Badu <david77115@gmail.com>
2019-07-01 15:54:32 +0800

    feat: create shiny mask activity

---
+++ app/src/main/java/com/crazyma/batuanimlab/shinymask/ShinyMaskActivity.kt
@@ -0,0 +1,11 @@
+package com.crazyma.batuanimlab.shinymask
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShinyMaskActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
