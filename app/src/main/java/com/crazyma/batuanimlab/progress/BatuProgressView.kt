b768d17f1085a6a449743279de25a8b90dcf2562
Badu <david77115@gmail.com>
2019-07-08 17:25:25 +0800

    test: draw line

--- app/src/main/java/com/crazyma/batuanimlab/progress/BatuProgressView.kt
+++ app/src/main/java/com/crazyma/batuanimlab/progress/BatuProgressView.kt
@@ -1,6 +1,9 @@
 package com.crazyma.batuanimlab.progress
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -10,6 +13,15 @@ class BatuProgressView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = 40f
+        color = Color.BLACK
+    }
 
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawLine(30f,60f,width.toFloat() - 30f,60f, paint)
+    }
 
 }
