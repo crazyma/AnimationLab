2306e608e32922cea6af212dec9cd46572fb3cd4
Badu <david77115@gmail.com>
2019-07-15 18:22:31 +0800

    refactor: setup ClipView and inset of content layout in Queue Page

--- app/src/main/java/com/crazyma/batuanimlab/queue/QueueActivity.kt
+++ app/src/main/java/com/crazyma/batuanimlab/queue/QueueActivity.kt
@@ -1,14 +1,29 @@
 package com.crazyma.batuanimlab.queue
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import com.crazyma.batuanimlab.R
+import kotlinx.android.synthetic.main.activity_queue.*
 
 class QueueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_queue)
+
+        this.findViewById<View>(android.R.id.content).systemUiVisibility =
+            (View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+
+        ViewCompat.setOnApplyWindowInsetsListener(queueLayout) { v, insets ->
+            val params = v.layoutParams as ViewGroup.MarginLayoutParams
+            params.topMargin = insets.systemWindowInsetTop
+            params.bottomMargin = insets.systemWindowInsetBottom
+            insets.consumeSystemWindowInsets()
+        }
+
     }
 
 }
