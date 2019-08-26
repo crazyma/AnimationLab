0fe864d953a9ff6e65ffd580a8e7722531b40158
Badu <david77115@gmail.com>
2019-08-26 14:44:26 +0800

    refactor: tune PartialLeftRoundCrop

--- app/src/main/java/com/crazyma/batuanimlab/crop_row/CropRowActivity.kt
+++ app/src/main/java/com/crazyma/batuanimlab/crop_row/CropRowActivity.kt
@@ -13,15 +13,36 @@ class CropRowActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crop_row)
 
-
-
-
-        Glide.with(imageView)
+        Glide.with(imageView1)
             .load("https://gcs.dcard.tw/reaction/aa0d425f-d530-4478-9a77-fe3aedc79eea1539599257655.png")
             .apply(RequestOptions.bitmapTransform(PartialLeftRoundCrop()))
-            .into(imageView)
+            .into(imageView1)
+
+        Glide.with(imageView2)
+            .load("https://gcs.dcard.tw/reaction/514c2569-fd53-4d9d-a415-bf0f88e7329f1539599270972.png")
+            .apply(RequestOptions.bitmapTransform(PartialLeftRoundCrop()))
+            .into(imageView2)
+
+        Glide.with(imageView3)
+            .load("https://gcs.dcard.tw/reaction/011ead16-9b83-4729-9fde-c588920c6c2d1539599284385.png")
+            .apply(RequestOptions.bitmapTransform(PartialLeftRoundCrop()))
+            .into(imageView3)
+
+        Glide.with(imageView4)
+            .load("https://gcs.dcard.tw/reaction/4b018f48-e184-445f-adf1-fc8e04ba09b91539675779141.png")
+            .apply(RequestOptions.bitmapTransform(PartialLeftRoundCrop()))
+            .into(imageView4)
+
+        Glide.with(imageView5)
+            .load("https://gcs.dcard.tw/reaction/e8e6bc5d-41b0-4129-b134-97507523d7ff1539599242687.png")
+            .apply(RequestOptions.bitmapTransform(PartialLeftRoundCrop()))
+            .into(imageView5)
 
 
+        Glide.with(imageView6)
+            .load("https://gcs.dcard.tw/reaction/286f599c-f86a-4932-82f0-f5a06f1eca031539599210825.png")
+            .apply(RequestOptions.bitmapTransform(PartialLeftRoundCrop()))
+            .into(imageView6)
     }
 
 }
--- app/src/main/java/com/crazyma/batuanimlab/crop_row/PartialLeftRoundCrop.kt
+++ app/src/main/java/com/crazyma/batuanimlab/crop_row/PartialLeftRoundCrop.kt
@@ -1,6 +1,6 @@
 package com.crazyma.batuanimlab.crop_row
 
-import android.graphics.Bitmap
+import android.graphics.*
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
 import java.security.MessageDigest
@@ -8,7 +8,7 @@ import java.security.MessageDigest
 class PartialLeftRoundCrop : BitmapTransformation() {
 
     override fun transform(pool: BitmapPool, toTransform: Bitmap, outWidth: Int, outHeight: Int): Bitmap {
-        return toTransform
+        return createCroppedBitmap(toTransform)
     }
 
     override fun updateDiskCacheKey(messageDigest: MessageDigest) {
@@ -25,9 +25,25 @@ class PartialLeftRoundCrop : BitmapTransformation() {
     }
 
     companion object {
-        //  TODO by Batu: change id value
         private const val ID = "com.bumptech.glide.transformations.FillSpace"
         private val ID_BYTES = ID.toByteArray(charset(STRING_CHARSET_NAME))
+    }
+
+    private val paint = Paint().apply {
+        isAntiAlias = true
+        style = Paint.Style.FILL
+        color = Color.YELLOW
+        xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
+    }
+
+    private fun createCroppedBitmap(originBitmap: Bitmap): Bitmap {
+        val finalBitmap = Bitmap.createBitmap(originBitmap.width, originBitmap.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(finalBitmap)
+
+        canvas.drawBitmap(originBitmap, 0f, 0f, null)
+        canvas.drawCircle(originBitmap.width * -0.37f, originBitmap.height * 0.5f, originBitmap.width * 0.5f, paint)
+
+        return finalBitmap
     }
 
 }
