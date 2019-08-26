67919b55e0446df195f85a1aa8a0a7ad71e7fb2d
Badu <david77115@gmail.com>
2019-08-26 13:11:05 +0800

    feat: create CropRowActivity

---
+++ app/src/main/java/com/crazyma/batuanimlab/crop_row/CropRowActivity.kt
@@ -0,0 +1,14 @@
+package com.crazyma.batuanimlab.crop_row
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.crazyma.batuanimlab.R
+
+class CropRowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crop_row)
+    }
+
+}
