4292782a33598103823b9198c0b71d679846fee4
Badu <david77115@gmail.com>
2019-10-18 15:03:25 +0800

    feat: create galaxy page

---
+++ app/src/main/java/com/crazyma/batuanimlab/inner_trans/GalaxyActivity.kt
@@ -0,0 +1,17 @@
+package com.crazyma.batuanimlab.inner_trans
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.crazyma.batuanimlab.R
+
+/**
+ * @author Batu
+ */
+class GalaxyActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_galaxy)
+    }
+
+}
