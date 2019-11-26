d1b2d2c6607d65720d16a9a44add3e6e34d4b43a
Badu <david77115@gmail.com>
2019-11-26 17:53:34 +0800

    feat: create BellyErrorView

---
+++ app/src/main/java/com/crazyma/batuanimlab/belly/BellyErrorView.kt
@@ -0,0 +1,18 @@
+package com.crazyma.batuanimlab.belly
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * @author Batu
+ */
+class BellyErrorView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+
+
+}
