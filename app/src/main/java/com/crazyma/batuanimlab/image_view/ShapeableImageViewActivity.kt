a3cc41be2eacf6b879eefea15a30a7bd5efbb8cf
Batu-Pro16 <david77115@gmail.com>
2020-08-21 11:53:04 +0800

    refactor: create ShapeableImageViewActivity

---
+++ app/src/main/java/com/crazyma/batuanimlab/image_view/ShapeableImageViewActivity.kt
@@ -0,0 +1,17 @@
+package com.crazyma.batuanimlab.image_view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.crazyma.batuanimlab.R
+
+/**
+ * @author Batu
+ */
+class ShapeableImageViewActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shapeable_image)
+    }
+
+}
