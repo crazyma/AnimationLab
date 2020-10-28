5e3200247e19e6b3c3fcaa5bbdb40db8a7f7ba23
Badu <david77115@gmail.com>
2020-10-28 11:29:28 +0800

    feat: create NotifyListActivity

---
+++ app/src/main/java/com/crazyma/batuanimlab/notify_list/NotifyListActivity.kt
@@ -0,0 +1,17 @@
+package com.crazyma.batuanimlab.notify_list
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.crazyma.batuanimlab.R
+
+/**
+ * @author Batu
+ */
+class NotifyListActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notify_list)
+    }
+
+}
