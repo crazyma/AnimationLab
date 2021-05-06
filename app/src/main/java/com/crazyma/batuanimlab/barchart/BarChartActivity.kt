7dd76bdbc0a0c419c19cf76021e64b498296bfd2
Batu-Pro16 <david77115@gmail.com>
2021-05-06 17:01:36 +0800

    feat: setup date textview chain

--- app/src/main/java/com/crazyma/batuanimlab/barchart/BarChartActivity.kt
+++ app/src/main/java/com/crazyma/batuanimlab/barchart/BarChartActivity.kt
@@ -2,16 +2,21 @@ package com.crazyma.batuanimlab.barchart
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.crazyma.batuanimlab.R
+import com.crazyma.batuanimlab.databinding.ActivityBarChartBinding
 
 /**
  * @author Batu
  */
 class BarChartActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityBarChartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bar_chart)
+        binding = ActivityBarChartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.barChartView.setupDate()
     }
 
 }
